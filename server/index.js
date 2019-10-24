9e82676971f1b4690b43a3421687961a99e9c9db
Devin George <devin.k.george@gmail.com>
2019-10-24 15:26:24 -0500

    Add serving of static html file

--- server/index.js
+++ server/index.js
@@ -1,6 +1,18 @@
-const express = require('express')
-const app = express()
-app.get('/', (req, res) => {
-  res.send('HEY, HEY!')
-})
+// Dependencies
+const express = require('express');
+const http = require('http');
+const path = require('path');
+
+// Instantation
+const app = express();
+const port = 3005;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '/../public')));
+
+// Routes
+
+//Listen
 app.listen(3005, () => console.log('Server running on port 3005'))
